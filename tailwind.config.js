2fd50b685b091557f2bfd0cfab5d1ffe753c6576
Bruno Bertolini <dev@brunobertolini.com>
2020-06-04 20:55:34 -0300

    chore: add color theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,11 +2,15 @@ module.exports = {
   theme: {
     extend: {
       colors: {
+        lightGray: '#d6d6d6',
         gray: '#9D9FAA',
         jungle: '#1a1d28',
         gunmetal: '#2e3142',
         green: '#43bc70',
         red: '#F25F5C',
+      },
+      container: {
+        center: true,
       },
     },
   },
