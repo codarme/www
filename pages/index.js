1ff9a84e65a793351a103c6116bcf68786300cf9
Bruno Bertolini <dev@brunobertolini.com>
2020-06-16 20:19:25 -0300

    feat: show success message on form submit success

--- pages/index.js
+++ pages/index.js
@@ -6,6 +6,9 @@ import { Topbar } from '~/components/Topbar'
 import { Button } from '~/components/Button'
 import { Input } from '~/components/Input'
 
+import Congratulation from './congratulation.svg'
+import Email from './email.svg'
+
 const Presentation = () => (
   <div className="p-3 text-lightGray md:flex-1">
     <h1 className="p-1 text-3xl md:text-4xl lg:text-5xl">
@@ -18,7 +21,7 @@ const Presentation = () => (
   </div>
 )
 
-const Form = () => {
+const Form = ({ onSuccess }) => {
   const onSubmit = async (data) => {
     try {
       await axios({
@@ -26,6 +29,8 @@ const Form = () => {
         url: '/api/notify-me',
         data,
       })
+
+      onSuccess()
     } catch (error) {
       console.log(error)
     }
@@ -73,12 +78,40 @@ const Form = () => {
   )
 }
 
-const Jumbotron = () => (
-  <div className="max-w-5xl container p-2 flex-1 flex flex-col justify-center md:flex-row md:items-center md:p-4 lg:p-10">
-    <Presentation />
-    <Form />
+const Congratulations = () => (
+  <div className="p-4 bg-gunmetal rounded-lg text-white text-center flex flex-col md:flex-1 md:max-w-sm">
+    <h3 className="p-4 text-xl text-green font-bold">
+      <Congratulation height={30} className="inline px-6" />
+      <span>Parabéns!</span>
+      <Congratulation height={30} className="inline px-6" />
+    </h3>
+
+    <p className="p-4 font-bold leading-loose md:max-w-sm">
+      Esse foi um passo importante para você melhorar cada vez mais como
+      desenvolvedor!
+    </p>
+
+    <p className="p-4 flex items-center justify-center">
+      <Email height={25} className="fill-current inline px-3" color="white" />
+      <span>Verifique seu e-mail</span>
+    </p>
   </div>
 )
+
+const Jumbotron = () => {
+  const [formSubmitted, setSubmitted] = React.useState(false)
+
+  return (
+    <div className="max-w-5xl container p-2 flex-1 flex flex-col justify-center md:flex-row md:items-center md:p-4 lg:p-10">
+      <Presentation />
+      {formSubmitted ? (
+        <Congratulations />
+      ) : (
+        <Form onSuccess={() => setSubmitted(true)} />
+      )}
+    </div>
+  )
+}
 
 const Hero = () => (
   <div
