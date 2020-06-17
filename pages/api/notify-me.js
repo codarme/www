bd44cb554434ffa9eca902935a2da3ed33660af6
Bruno Bertolini <dev@brunobertolini.com>
2020-06-17 20:02:13 -0300

    refactor: split params to add context

--- pages/api/notify-me.js
+++ pages/api/notify-me.js
@@ -8,24 +8,24 @@ export default (req, res) => {
     return res.end()
   }
 
+  const onAddSuccess = (result) => {
+    res.statusCode = 200
+    res.setHeader('Content-Type', 'application/json')
+    res.json(result)
+  }
+
+  const options = [
+    {
+      email,
+      variables: { name },
+    },
+  ]
+
   sendpulse.init(
     process.env.SENDPULSE_KEY,
     process.env.SENDPULSE_SECRET,
     '/tmp/',
     (token) =>
-      sendpulse.addEmails(
-        (result) => {
-          res.statusCode = 200
-          res.setHeader('Content-Type', 'application/json')
-          res.end(JSON.stringify(result))
-        },
-        process.env.SENDPULSE_LIST_ID,
-        [
-          {
-            email,
-            variables: { name },
-          },
-        ]
-      )
+      sendpulse.addEmails(onAddSuccess, process.env.SENDPULSE_LIST_ID, options)
   )
 }
