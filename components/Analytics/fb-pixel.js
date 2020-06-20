86bb85aac73eafd4b1ab89e68a4d79951ba4e6d6
Bruno Bertolini <dev@brunobertolini.com>
2020-06-20 10:02:25 -0300

    fix: fb pixel init with many codes

--- components/Analytics/fb-pixel.js
+++ components/Analytics/fb-pixel.js
@@ -1,29 +1,29 @@
-export const FBPixel = ({ codes }) => (
-  <>
-    <script
-      dangerouslySetInnerHTML={{
-        __html: `
-              !function(f,b,e,v,n,t,s)
-              {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-              n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-              if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-              n.queue=[];t=b.createElement(e);t.async=!0;
-              t.src=v;s=b.getElementsByTagName(e)[0];
-              s.parentNode.insertBefore(t,s)}(window, document,'script',
-              'https://connect.facebook.net/en_US/fbevents.js');
-              ${codes.map((code) => `fbq('init', '${code}');`)}
-              fbq('track', 'PageView');`,
-      }}
-    />
-    <noscript
-      dangerouslySetInnerHTML={{
-        __html: `<img height="1" width="1" style="display:none" src="https://www.facebook.com/tr?id=2741984492744774&ev=PageView&noscript=1" />`,
-      }}
-    />
-    <noscript
-      dangerouslySetInnerHTML={{
-        __html: `<img height="1" width="1" style="display:none" src="https://www.facebook.com/tr?id=2741984492744774&ev=PageView&noscript=1" />`,
-      }}
-    />
-  </>
-)
+export const FBPixel = ({ codes }) => {
+  const initCodes = codes.split(',').map((code) => `fbq('init', '${code}');`)
+
+  return (
+    <>
+      <script
+        dangerouslySetInnerHTML={{
+          __html: `!function(f,b,e,v,n,t,s)
+            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+            n.queue=[];t=b.createElement(e);t.async=!0;
+            t.src=v;s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s)}(window, document,'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+            ${initCodes.join('')}
+            fbq('track', 'PageView');`,
+        }}
+      />
+      {codes.map((code) => (
+        <noscript
+          dangerouslySetInnerHTML={{
+            __html: `<img height="1" width="1" style="display:none" src="https://www.facebook.com/tr?id=${code}&ev=PageView&noscript=1" />`,
+          }}
+        />
+      ))}
+    </>
+  )
+}
--- components/Analytics/index.js
+++ components/Analytics/index.js
@@ -4,6 +4,6 @@ import { FBPixel } from './fb-pixel'
 export const Analytics = ({ children }) => (
   <>
     <GATag code={process.env.GA_TAG} />
-    <FBPixel codes={process.env.FB_PIXEL_IDS.split(',')} />
+    <FBPixel codes={process.env.FB_PIXEL_IDS} />
   </>
 )
